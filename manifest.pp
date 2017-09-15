f482f116f2341d953520b57f9ce8cb31cb40f3d4
Tarnatos <tarnatos@posteo.de>
2017-09-15 13:55:10 +0200

    Anapssungen für Netsplit AHB

--- manifest.pp
+++ manifest.pp
@@ -1,5 +1,5 @@
 class { 'ffnord::params':
-  router_id => "10.187.$$$.$$$",  # The id of this router, probably the ipv4 address
+  router_id => "10.187.10$.$$$",  # The id of this router, probably the ipv4 address
                                   # of the mesh device of the providing community
   icvpn_as => "65187",            # The as of the providing community
   wan_devices => ['eth0'],        # An array of devices which should be in the wan zone
@@ -9,12 +9,11 @@ class { 'ffnord::params':
   rmem_default => 87380,          # Define the default socket recv buffer
   rmem_max     => 12582912,       # Define the maximum socket recv buffer
   
-  gw_control_ips => "217.70.197.1 89.27.152.1 138.201.16.163 8.8.8.8", # Define target to ping against for function check
+  gw_control_ips => "8.8.8.8", # Define target to ping against for function check
 
   max_backlog  => 5000,           # Define the maximum packages in buffer
   include_bird4 => false,
   maintenance => 0,
-  #debian_mirror => "http://repo.myloc.de/mirrors/ftp.de.debian.org/debian/";
   
   batman_version => 15,            # B.A.T.M.A.N. adv version
 }
@@ -26,19 +25,19 @@ ffnord::mesh { 'mesh_ffnord':
   , mesh_as => "65187"
   , mesh_mac  => "fe:ed:be:ef:ff:$$"
   , vpn_mac  => "fe:ed:be:ff:ff:$$"
-  , mesh_ipv6 => "2a03:2267:4e6f:7264::fd$$/64"
-  , mesh_ipv4  => "10.187.$$$.$$$/17"
+  , mesh_ipv6 => "fd42:eb49:c0b5:4242::ff$$/64"
+  , mesh_ipv4  => "10.187.10$.$$$/17"
   , range_ipv4 => "10.187.0.0/16"
   , mesh_mtu     => "1280"
   , mesh_peerings    => "/root/mesh_peerings.yaml"
   
-  , fastd_secret => "/root/nord-gw$$-fastd-secret.key"
+  , fastd_secret => "/root/nord-ahb-gw$$-fastd-secret.key"
   , fastd_port   => 10050
-  , fastd_peers_git => 'https://github.com/Freifunk-Nord/nord-gw-peers.git'
+  , fastd_peers_git => 'https://github.com/Freifunk-AHB/nord-ahb-gw-peers.git'
   , fastd_verify=> 'true'                               # set this to 'true' to accept all fastd keys without verification
   
-  , dhcp_ranges => ['10.187.$$$.2 10.187.$$$.254'] 
-  , dns_servers => ['10.187.$$$.$$$']               # should be the same as $router_id
+  , dhcp_ranges => ['10.187.10$.2 10.187.10$.254'] 
+  , dns_servers => ['10.187.10$.$$$']               # should be the same as $router_id
 }
 
 class {'ffnord::vpn::provider::hideio':
@@ -49,30 +48,12 @@ class {'ffnord::vpn::provider::hideio':
 }
 
 ffnord::named::zone {
-  "nord": zone_git => "https://github.com/Freifunk-Nord/nord-zone.git", exclude_meta => 'nord';
-}
-
-ffnord::icvpn::setup {
-  'nordgw$$':
-  icvpn_as => 65187,
-  icvpn_ipv4_address => "10.207.$$$.$$$",
-  icvpn_ipv6_address => "fec0::a:cf:$$$:$$$",
-  icvpn_exclude_peerings     => [Nord],
-  tinc_keyfile       => "/root/nord-vpn$$-icvpn-rsa_key.priv"
+  "nord": zone_git => "https://github.com/Freifunk-AHB/nord-ahb-zone.git", exclude_meta => 'nord';
 }
 
 class {
   ['ffnord::etckeeper','ffnord::rsyslog','ffnord::mosh','ffnord::alfred']:
 }
-
-#class {
-#  'ffnord::monitor::nrpe':
-#     allowed_hosts => "217.70.197.95";
-#}
-#class {
-#  'ffnord::monitor::zabbix':
-#    zabbixserver => "5.9.51.89";
-#}
 
 # Useful packages
 package {
